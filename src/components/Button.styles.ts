f4121ff3b79a0551b494a7649f2d06d63aaf5e31
tuiusx <tuiusx@icloud.com>
2023-03-16 19:21:39 -0300

    Application structure - Colors & font

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -16,8 +16,14 @@ const buttonVariant = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 40px;
+    border-radius: 4px;
+    border: 0;
+    margin: 8px;
 
-    ${props => {
+    background-color: ${props => props.theme['green-500']};
+    color: ${props => props.theme.white};
+
+    /* ${props => {
         return css`background-color: ${buttonVariant[props.variant]}`
-    }}
+    }} */
 `
