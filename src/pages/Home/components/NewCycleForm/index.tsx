f4faf4e6522a8fdc57d0ef3953a4483602bbdab2
tuiusx <tuiusx@icloud.com>
2023-04-05 17:52:16 -0300

    Application features - Separating components

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,37 @@
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+
+export function NewCycleForm() {
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        list="task-suggestions"
+        placeholder="Dê um nome para o seu projeto"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="projeto 1" />
+        <option value="projeto 2" />
+        <option value="projeto 3" />
+        <option value="banana" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={5}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
