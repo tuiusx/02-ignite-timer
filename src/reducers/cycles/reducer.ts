4f4831f872667cf9446066f41c39440e50d1e8ad
Tuiusx <tuiusx@icloud.com>
2023-08-01 12:55:24 -0300

    Reducers - Saving state to storage

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -16,7 +16,7 @@ interface CyclesState {
   activeCycleId: string | null
 }
 
-export function cyclesReducaer(state: CyclesState, action: any) {
+export function cyclesReducer(state: CyclesState, action: any) {
   switch (action.type) {
     case ActionTypes.ADD_NEW_CYCLE:
       return produce(state, (draft) => {
